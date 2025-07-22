6590cb014744534d2d20989e5a939e0ef24ff4fc
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2025-07-22 22:40:48 +0200

    Implemented update functionality

--- obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b359b68c31000fff615cc6c948783a9a9e921671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d82dd2b4330956c822cfd7e536f6d888f0ae50a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
