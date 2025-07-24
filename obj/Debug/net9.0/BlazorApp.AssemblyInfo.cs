e8d3ce83acffd591a6ccd0cadd43bfdf71bf1487
Basswell Mbilima <basswellbiotechmbilima@gmail.com>
2025-07-24 08:58:35 +0200

    a comit

--- obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d3719f7c3c58a02e32ede0998244db3c560c140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae58040d22da5ed9fcc481493f73bc1fa7dbb892")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
